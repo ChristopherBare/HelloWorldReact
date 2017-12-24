dfc1810161533d6c14135d108e5f09412f188b46
Christopher Bare <christopherbare@outlook.com>
2017-12-24 14:24:52 -0500

    Added files for Project.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">Hello, World!</h1>
+            </header>
+
+            <p className="App-intro">My first React App.
+                It does absolutely nothing, except look at that
+                thing at the top spin! Isn't it beautifully
+                <strong> hypnotic</strong>?
+            </p>
+        </div>;
+    }
+}
+
+
+export default App;
